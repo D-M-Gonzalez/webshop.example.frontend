f23354861a105c372bafc3bd265e166e9207341d
Damian Martin Gonzalez <dami.m.gonza@gmail.com>
2022-03-15 18:23:08 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import React from "react";
+import {Routes, Route} from "react-router-dom";
+import Products from "./container/Products/Products";
+import Contact from "./container/Contact/Contact";
+import Home from "./container/Home/Home";
+import Layout from "./components/Layout";
+import Error404 from "./components/Error404";
 
-function App() {
-  return (
-    <div className="App">
-      Hola Mundo!
-    </div>
-  );
+export default function App() {
+    return (
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route path="products" element={<Products />}/>
+                    <Route path="contact" element ={<Contact />}/>
+                    <Route path="home/*" element={<Home />}>                           
+                    </Route>
+                    <Route path="*" element ={<Error404 />}/>
+                </Route>
+            </Routes>
+    );
 }
-
-export default App;
